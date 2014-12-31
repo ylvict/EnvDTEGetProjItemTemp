34cb5fb232903c2bd98f3ee52f1dd2e6ade09998
ylvict <ylvict@gmail.com>
2014-12-31 01:01:17 -0800

    Add Project source

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using EnvDTE90;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnvDTEGetProjItemTemp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dte = (EnvDTE.DTE)System.Runtime.InteropServices.Marshal.GetActiveObject("VisualStudio.DTE");
+            var temps = ((Solution3)dte.Solution).GetProjectItemTemplates("FSharp", null);
+            for (int i = 1; i <= temps.Count; i++)
+            {
+                Console.WriteLine(temps[i].Name);
+            }
+        }
+    }
+}
